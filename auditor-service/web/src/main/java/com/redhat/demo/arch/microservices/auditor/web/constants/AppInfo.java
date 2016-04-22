0f7519e4e88fff6fbcfa3a81bdeaadda02c83f6b
fabio.ercoli <fercoli@redhat.com>
2016-04-22 12:21:55 +0200

    auditor module (clone from history-module)

---
+++ auditor-service/web/src/main/java/com/redhat/demo/arch/microservices/auditor/web/constants/AppInfo.java
@@ -0,0 +1,41 @@
+/*
+ *
+ */
+package com.redhat.demo.arch.microservices.auditor.web.constants;
+
+/**
+ * The Enum AppInfo.
+ *
+ * @author Andrea Battaglia (Red Hat)
+ */
+public enum AppInfo {
+
+    /** The name. */
+    NAME("HISTORY Service"),
+
+    /** The version. */
+    VERSION("1.0.0");
+
+    /** The value. */
+    private final String value;
+
+    /**
+     * Instantiates a new app info.
+     *
+     * @param value the value
+     */
+    private AppInfo(String value) {
+        this.value = value;
+    }
+
+    /**
+     * Gets the value.
+     *
+     * @return the value
+     */
+    public String getValue() {
+        return value;
+    }
+
+
+}
